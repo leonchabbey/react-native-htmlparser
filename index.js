82ad7fc57a28bc7bc911098eca7b4f4f8b0cec71
Gustavo Fão Valvassori <faogustavo@gmail.com>
2017-02-15 14:33:28 -0200

    Some refactoring
    * Refactored methods to ESLint specification
    * Added encapsulate property
    * Added propTypes

--- index.js
+++ index.js
@@ -1,75 +1,24 @@
-import parse5 from 'react-native-parse-html'
-import React, {Component} from "react"
+import parse5 from 'react-native-parse-html';
+import React, { Component, PropTypes } from 'react';
 import {
   StyleSheet,
   Text,
   View,
-} from 'react-native'
+} from 'react-native';
 
-var BLOCK_ELEMENTS = [
-  "address",
-  "blockquote",
-  "center",
-  "dir",
-  "div",
-  "dl",
-  "fieldset",
-  "form",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "hr",
-  "isindex",
-  "menu",
-  "noframes",
-  "noscript",
-  "ol",
-  "p",
-  "pre",
-  "table",
-  "ul",
-  "li"
-]
+const BLOCK_ELEMENTS = [
+  'address', 'blockquote', 'center', 'dir', 'div', 'dl', 'fieldset', 'form',
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'isindex', 'menu', 'noframes',
+  'noscript', 'ol', 'p', 'pre', 'table', 'ul', 'li'
+];
 
-var INLINE_ELEMENTS = [
-  "a",
-  "abbr",
-  "acronym",
-  "b",
-  "basefont",
-  "bdo",
-  "big",
-  "br",
-  "cite",
-  "code",
-  "dfn",
-  "em",
-  "font",
-  "i",
-  "img",
-  "input",
-  "kbd",
-  "label",
-  "q",
-  "s",
-  "samp",
-  "select",
-  "small",
-  "span",
-  "strike",
-  "strong",
-  "sub",
-  "sup",
-  "textarea",
-  "tt",
-  "u",
-  "var"
-]
+const INLINE_ELEMENTS = [
+  'a', 'abbr', 'acronym', 'b', 'basefont', 'bdo', 'big', 'br', 'cite', 'code',
+  'dfn', 'em', 'font', 'i', 'img', 'input', 'kbd', 'label', 'q', 's', 'samp',
+  'select', 'small', 'span', 'strike', 'strong', 'sub', 'sup', 'textarea', 'tt', 'u', 'const'
+];
 
-var DEFAULT_STYLES = {
+const DEFAULT_STYLES = StyleSheet.create({
   a: {
 
   },
@@ -86,7 +35,7 @@ var DEFAULT_STYLES = {
 
   },
   code: {
-    fontFamily: "Courier"
+    fontFamily: 'Courier'
   },
   div: {
 
@@ -95,22 +44,22 @@ var DEFAULT_STYLES = {
     fontStyle: 'italic'
   },
   h1: {
-    fontWeight: 'bold',
+    fontSize: 24,
   },
   h2: {
-    fontWeight: 'bold',
+    fontSize: 20,
   },
   h3: {
-    fontWeight: 'bold',
+    fontSize: 18,
   },
   h4: {
-    fontWeight: 'bold',
+    fontSize: 17,
   },
   h5: {
-    fontWeight: 'bold',
+    fontSize: 16,
   },
   h6: {
-    fontWeight: 'bold',
+    fontSize: 15,
   },
   i: {
     fontStyle: 'italic'
@@ -136,117 +85,107 @@ var DEFAULT_STYLES = {
   sup: {
 
   },
-  ol:{
+  ol: {
     marginLeft: 24,
   },
   ul: {
     marginLeft: 24,
   },
   u: {
-    textDecorationLine: "underline"
+    textDecorationLine: 'underline'
   },
   default: {
     marginBottom: 12
   }
-}
+});
+
+const propTypes = {
+  encapsulate: PropTypes.bool,
+  html: PropTypes.string,
+  tagsStyle: PropTypes.instanceOf(StyleSheet)
+};
 
 class HtmlParser extends Component {
-  parse = (html) => {
-    var fragment = parse5.parseFragment(html)
-    return fragment
+
+  parse = (html) => parse5.parseFragment(html);
+  isText = (tagName) => tagName === '#text';
+  isBlockElement = tagName => BLOCK_ELEMENTS.indexOf(tagName) !== -1;
+  isInlineElement = tagName => INLINE_ELEMENTS.indexOf(tagName) !== -1
+  isEmpty = node => node.value.trim() === ''
+
+  styleForTag(tagName) {
+    const { tagsStyle } = this.props;
+    const styles = [];
+
+    if (DEFAULT_STYLES[tagName]) {
+      styles.push(DEFAULT_STYLES[tagName]);
+    } else {
+      styles.push(DEFAULT_STYLES.default);
+    }
+
+    if (tagsStyle && tagsStyle.general) {
+      styles.push(tagsStyle.general);
+    }
+
+    if (tagsStyle && tagsStyle[tagName]) {
+      styles.push(tagsStyle[tagName]);
+    }
+
+    return styles;
   }
 
-  isText = (tagName) => {
-    return tagName === "#text"
-  }
-
-  isBlockElement = (tagName) => {
-    return BLOCK_ELEMENTS.indexOf(tagName) != -1
-  }
-
-  isInlineElement = (tagName) => {
-    return INLINE_ELEMENTS.indexOf(tagName) != -1
-  }
-
-  isEmpty = (node) => {
-    return node.value.trim() == ""
-  }
-
-  styleForTag = (tagName) => {
-    const {tagsStyle} = this.props
-
-    var style = tagsStyle
-                  ? tagsStyle[tagName]
-                    ? tagsStyle[tagName]
-                    : DEFAULT_STYLES[tagName]
-                      ? DEFAULT_STYLES[tagName]
-                      : DEFAULT_STYLES["default"]
-                  : DEFAULT_STYLES[tagName]
-                    ? DEFAULT_STYLES[tagName]
-                    : DEFAULT_STYLES["default"]
-
-    return tagsStyle
-            ? tagsStyle["general"]
-              ? [tagsStyle["general"], style]
-              : style
-            : style
-  }
-  processNode = (node, parentKey) => {
-    var nodeName = node.nodeName
+  processNode(node, parentKey) {
+    const nodeName = node.nodeName;
 
     if (this.isText(nodeName)) {
-      if (this.isEmpty(node)) {
-        return null
-      }
-
-      var key = `${parentKey}_text`
-      return (<Text key={key}>{node.value}</Text>)
+      const key = `${parentKey}_text`;
+      return (<Text key={key}>{node.value}</Text>);
     }
 
     if (this.isInlineElement(nodeName)) {
-      var key = `${parentKey}_${nodeName}`
-      var children = []
+      const key = `${parentKey}_${nodeName}`;
+      const children = [];
       node.childNodes.forEach((childNode, index) => {
         if (this.isInlineElement(childNode.nodeName) || this.isText(childNode.nodeName)) {
-          children.push(this.processNode(childNode, `${key}_${index}`))
+          children.push(this.processNode(childNode, `${key}_${index}`));
         } else {
-          console.error(`Inline element ${nodeName} can only have inline children, ${child} is invalid!`)
+          console.error(`Inline element ${nodeName} can only have inline children, ${child} is invalid!`);
         }
-      })
-      return (<Text key={key} style={this.styleForTag(nodeName)}>{children}</Text>)
+      });
+
+      return (<Text key={key} style={this.styleForTag(nodeName)}>{children}</Text>);
     }
 
     if (this.isBlockElement(nodeName)) {
-      var key = `${parentKey}_${nodeName}`
-      var children = []
-      var lastInlineNodes = []
+      const key = `${parentKey}_${nodeName}`;
+      const children = [];
+      let lastInlineNodes = [];
 
       node.childNodes.forEach((childNode, index) => {
-        var child = this.processNode(childNode, `${key}_${index}`)
+        const child = this.processNode(childNode, `${key}_${index}`);
         if (this.isInlineElement(childNode.nodeName) || this.isText(childNode.nodeName)) {
-          lastInlineNodes.push(child)
-
+          lastInlineNodes.push(child);
         } else if (this.isBlockElement(childNode.nodeName)) {
           if (lastInlineNodes.length > 0) {
-            children.push(<Text key={`${key}_${index}_inline`}>{lastInlineNodes}</Text>)  
-            lastInlineNodes = []
+            children.push(<Text key={`${key}_${index}_inline`}>{lastInlineNodes}</Text>)
+            lastInlineNodes = [];
           }
-          children.push(child)
+          children.push(child);
         }
-      })
+      });
 
       if (lastInlineNodes.length > 0) {
-        children.push((<Text key={`${key}_last_inline`}>{lastInlineNodes}</Text>))  
+        children.push((<Text key={`${key}_last_inline`}>{lastInlineNodes}</Text>))
       }
 
       return (
         <Text key={key} style={this.styleForTag(nodeName)}>
-          {nodeName === "li"
-            ? <Text>&bull; <Text>{children}</Text>{"\n"}</Text>
+          {nodeName === 'li'
+            ? <Text>&bull; <Text>{children}</Text>{'\n'}</Text>
             : children
           }
         </Text>
-      )
+      );
     }
 
     console.warn(`unsupported node: ${nodeName}`)
@@ -254,21 +193,23 @@ class HtmlParser extends Component {
   }
 
   render() {
-    var html = this.props.html
-    var fragment = this.parse(html)
-    var rootKey = "ht_"
+    const html = this.props.html;
+    const fragment = this.parse(this.props.encapsulate ? `<p>${html}</p>` : html);
+    const rootKey = 'ht_';
 
-    var children = []
+    const children = [];
     fragment.childNodes.forEach((node, index) => {
-      children.push(this.processNode(node, `${rootKey}_${index}`))
-    })
+      children.push(this.processNode(node, `${rootKey}_${index}`));
+    });
 
     return (
       <View style={this.props.containerStyle}>
         {children}
       </View>
-    )
+    );
   }
 }
 
-export default HtmlParser
+HtmlParser.propTypes = propTypes;
+
+export default HtmlParser;
